952255e7db442d10a7eb71024c92e08ef0122a73
MattGuerrette <direct3Dtutorials@gmail.com>
2017-03-17 21:26:39 -0400

    added licensing

--- source/library.cpp
+++ source/library.cpp
@@ -1,3 +1,16 @@
+/**
+**    Hatchit Engine
+**    Copyright(c) 2017 Third-Degree
+**
+**    GNU Lesser General Public License
+**    This file may be used under the terms of the GNU Lesser
+**    General Public License version 3 as published by the Free
+**    Software Foundation and appearing in the file LICENSE included
+**    in the packaging of this file. Please review the following information
+**    to ensure the GNU Lesser General Public License requirements
+**    will be met: https://www.gnu.org/licenses/lgpl.html
+**
+**/
 
 #include <library.h>
 #include <pkh_platform.h>
